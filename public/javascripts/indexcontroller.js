8e69c4124ee2898aa67bebe9e7e4cb6e91abaa6a
Robbie Netzorg <robbizorg@gmail.com>
2016-02-06 04:18:07 -0500

    Added Graph, Changed Background in Analysis

--- public/javascripts/indexcontroller.js
+++ public/javascripts/indexcontroller.js
@@ -1,8 +1,27 @@
 
-var app = angular.module('picpop', []);
+var app = angular.module('picpop', ["chart.js"]);
 
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      colours: ['#FF5252', '#FF8A80'],
+      responsive: false
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('Line', {
+      datasetFill: false
+    });
+}])
 app.controller('MainCtrl', function($scope, $window, $http) {
     $scope.firstName = "Eshan";
+
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+
+    $scope.info = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
 
     $scope.authenticate = function() {
 
@@ -97,7 +116,7 @@ app.controller('MainCtrl', function($scope, $window, $http) {
                 }
             }
         });
-
+    }
 
 
     $scope.generateKey = function() {
